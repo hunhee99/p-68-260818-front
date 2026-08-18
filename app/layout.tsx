7dfc4b0638e6622b9fa1a1f5dd898ba56739d496
HunheeChoi <67158609+hunhee99@users.noreply.github.com>
2026-08-18 11:42:31 +0900

    layout.tsx 헤더 메인 푸터로 분리

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,21 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <h1>layout header</h1>
+        
+        <header>
+          <nav className="flex gap-4">
+            <a href="/">메인</a>
+            <a href="/posts">글 목록</a>
+          </nav>
+        </header>
+        
+        <main>
         {children}
+        </main>
+
+
+        <footer>푸터</footer>
+
         </body>
     </html>
   );
