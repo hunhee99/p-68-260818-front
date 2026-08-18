e40b40cfe9b98c7342031952ad14910348976a82
HunheeChoi <67158609+hunhee99@users.noreply.github.com>
2026-08-18 12:22:44 +0900

    layout.tsx main에 flex-1

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="ko"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col !bg-red-300 border-2">
 
         <header>
           <nav className="flex gap-4">
@@ -33,7 +33,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           </nav>
         </header>
         
-        <main>
+        <main className="flex-1">
         {children}
         </main>
 
